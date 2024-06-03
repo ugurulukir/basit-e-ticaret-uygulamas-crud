0ecffbedf4f57c2f4143ea223db7afe8ed89af48
Uğur ULUKIR <u.ulukir@gmail.com>
2024-06-04 00:53:18 +0300

    models klasörüne products.cs sınıfı eklendi

---
+++ basit e-ticaret uygulaması crud/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace basit_e_ticaret_uygulaması_crud.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+        [MaxLength(100)]
+        public string Brand { get; set; } = "";
+        [MaxLength(100)]
+        public string Category { get; set; } = "";
+        [Precision(16,2)]
+        public decimal Price { get; set; }  
+        public string Description { get; set; } = "";
+        [MaxLength(100)]
+        public string ImageFileName { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
