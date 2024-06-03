b6a764203ff901c33ffe493d058cb34a62232c43
Uğur ULUKIR <u.ulukir@gmail.com>
2024-06-04 00:13:40 +0300

    Add project files.

---
+++ basit e-ticaret uygulaması crud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace basit_e_ticaret_uygulaması_crud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
