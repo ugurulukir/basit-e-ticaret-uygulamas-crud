c524a95baa3512f3f09882d4e29faa7ac827285b
Uğur ULUKIR <u.ulukir@gmail.com>
2024-06-04 15:04:58 +0300

    ürün görselinin kaydedilmesi için gerekli kodlar yazıldı

--- basit e-ticaret uygulaması crud/Controllers/ProductsController.cs
+++ basit e-ticaret uygulaması crud/Controllers/ProductsController.cs
@@ -1,15 +1,19 @@
 ﻿using basit_e_ticaret_uygulaması_crud.Models;
 using basit_e_ticaret_uygulaması_crud.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace basit_e_ticaret_uygulaması_crud.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext context;
-        public ProductsController(ApplicationDbContext context)
+        private readonly IWebHostEnvironment environment;
+
+        public ProductsController(ApplicationDbContext context, IWebHostEnvironment environment)
         {
             this.context = context;
+            this.environment = environment;
         }
         public IActionResult Index()
         {
@@ -30,7 +34,20 @@ namespace basit_e_ticaret_uygulaması_crud.Controllers
             if (!ModelState.IsValid) {
             return View(productDto);
             }
+
+            //save the image file
+
+            string newFileName = DateTime.Now.ToString("yyyymmddHHmmssfff");
+            newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+
+            string imageFullPath = environment.WebRootPath + "/products/" + newFileName;
+            using(var stream=System.IO.File.Create(imageFullPath))
+            {
+                productDto.ImageFile.CopyTo(stream);
+            }
             return RedirectToAction("Index", "Products");
         }
+
+
     }
 }
