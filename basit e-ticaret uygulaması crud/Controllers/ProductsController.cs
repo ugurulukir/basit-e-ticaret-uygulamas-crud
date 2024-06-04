5041b0f4e361d10542c6f5f60023d23b16947ed0
Uğur ULUKIR <u.ulukir@gmail.com>
2024-06-04 12:37:37 +0300

    product controller ve view eklendi

---
+++ basit e-ticaret uygulaması crud/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using basit_e_ticaret_uygulaması_crud.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace basit_e_ticaret_uygulaması_crud.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+    }
+}
