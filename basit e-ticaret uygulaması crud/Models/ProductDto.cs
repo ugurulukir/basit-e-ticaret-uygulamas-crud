670b88a5706c5c5ad868ea23da80897b269c2e27
Uğur ULUKIR <u.ulukir@gmail.com>
2024-06-04 13:08:23 +0300

    ProductDto modeli oluşturuldu

---
+++ basit e-ticaret uygulaması crud/Models/ProductDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace basit_e_ticaret_uygulaması_crud.Models
+{
+    public class ProductDto
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = "";
+        [Required, MaxLength(100)]
+        public string Brand { get; set; } = "";
+        [Required, MaxLength(100)]
+        public string Category { get; set; } = "";
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Description { get; set; } = "";
+        [MaxLength(100)]
+        public IFormFile? ImageFile { get; set; }
+    }
+}
